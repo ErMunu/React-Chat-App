c7d25df53ea70738a53d7f1ed8c9b5762f099b42
Master Muntazir <me@muntazir.in>
2022-12-03 23:35:22 +0530

    minor fix

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCVXhwmq705CPE455smvUXY2wYgOyA-xBE",
-    authDomain: "react-chat-app-cab52.firebaseapp.com",
-    projectId: "react-chat-app-cab52",
-    storageBucket: "react-chat-app-cab52.appspot.com",
-    messagingSenderId: "797511151173",
-    appId: "1:797511151173:web:57c0fb138b7d22ab53fd2c",
-    measurementId: "G-B1MR9B58CG"
+    // Add your firebase credentials
 };
 
 const app = firebase.initializeApp(firebaseConfig);
